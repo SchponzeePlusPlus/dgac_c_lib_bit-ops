ab512180e74263aa45fdbb41f26e0aa15b81853b
SchponzeePlusPlus <61894715+SchponzeePlusPlus@users.noreply.github.com>
2025-10-05 17:32:29 +1100

    Update dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
    - Forgot to update u32_clearVal usage

--- mod_binary-bitwise-ops_gen/dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
+++ mod_binary-bitwise-ops_gen/dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
@@ -19,6 +19,6 @@ uint32_t setShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_setVal, uint
 uint32_t clearShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_clearVal, uint8_t u8_shiftLeftAmt)
 {
 	uint32_t result = u32_inVal;
-	result &= ~(u32_setVal << u8_shiftLeftAmt);
+	result &= ~(u32_clearVal << u8_shiftLeftAmt);
 	return result;
 }
