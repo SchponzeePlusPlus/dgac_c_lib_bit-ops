ed44f6a4b802bf19b4bdce9b7aabbf4298c6e2d2
SchponzeePlusPlus <61894715+SchponzeePlusPlus@users.noreply.github.com>
2025-10-05 18:54:27 +1100

    Update dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
    - New functions and changes thanks to https://stackoverflow.com/questions/47981/how-to-set-clear-and-toggle-a-single-bit

--- mod_binary-bitwise-ops_gen/dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
+++ mod_binary-bitwise-ops_gen/dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
@@ -3,22 +3,59 @@
  * 
  * Author:
  * 
+ * Note: Credit to https://stackoverflow.com/questions/47981/how-to-set-clear-and-toggle-a-single-bit
+ * for improvements
  */
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.h"
 
 uint32_t setShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_setVal, uint8_t u8_shiftLeftAmt)
 {
+	/*
+	could do it like this, would look more like normal syntax:
 	uint32_t result = u32_inVal;
 	result |= (u32_setVal << u8_shiftLeftAmt);
 	return result;
+	*/
+	// This is more efficient
+	return (u32_inVal | (u32_setVal << u8_shiftLeftAmt));
 }
 
 uint32_t clearShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_clearVal, uint8_t u8_shiftLeftAmt)
 {
+	/*
+	could do it like this, would look more like normal syntax:
 	uint32_t result = u32_inVal;
 	result &= ~(u32_clearVal << u8_shiftLeftAmt);
 	return result;
+	*/
+	// This is more efficient
+	return (u32_inVal & ~(u32_clearVal << u8_shiftLeftAmt));
+}
+
+uint32_t togglShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_togglVal, uint8_t u8_shiftLeftAmt)
+{
+	/*
+	could do it like this, would look more like normal syntax:
+	uint32_t result = u32_inVal;
+	result ^= (u32_togglVal << u8_shiftLeftAmt);
+	return result;
+	*/
+	// This is more efficient
+	return (u32_inVal ^ (u32_togglVal << u8_shiftLeftAmt));
+}
+
+bool chkBitShiftRightValUint32(uint32_t u32_inVal, uint32_t u32_chkBitShiftdVal, uint8_t u8_shiftRightAmt)
+{
+	/*
+	could do it like this, would look more like normal syntax:
+	uint32_t result = ??;
+	result ?= ??;
+	return result;
+	*/
+	// This is more efficient
+	return ((u32_inVal >> u8_shiftRightAmt) & u32_chkBitShiftdVal);
 }
