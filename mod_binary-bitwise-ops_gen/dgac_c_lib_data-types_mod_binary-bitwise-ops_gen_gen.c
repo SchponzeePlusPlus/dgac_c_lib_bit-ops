bab2a6f70dabde3e9f25534c36a600b94bf5aacd
SchponzeePlusPlus <61894715+SchponzeePlusPlus@users.noreply.github.com>
2025-10-05 19:14:26 +1100

    Update dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
    - Updates on what functions haven't been tested / checked yet
    - new function for changing the nth bit to x , but not sure what use this function serves yet

--- mod_binary-bitwise-ops_gen/dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
+++ mod_binary-bitwise-ops_gen/dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
@@ -36,6 +36,7 @@ uint32_t clearShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_clearVal, 
 	return (u32_inVal & ~(u32_clearVal << u8_shiftLeftAmt));
 }
 
+// untested
 uint32_t togglShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_togglVal, uint8_t u8_shiftLeftAmt)
 {
 	/*
@@ -48,6 +49,7 @@ uint32_t togglShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_togglVal, 
 	return (u32_inVal ^ (u32_togglVal << u8_shiftLeftAmt));
 }
 
+// untested
 bool chkBitShiftRightValUint32(uint32_t u32_inVal, uint32_t u32_chkBitShiftdVal, uint8_t u8_shiftRightAmt)
 {
 	/*
@@ -58,4 +60,13 @@ bool chkBitShiftRightValUint32(uint32_t u32_inVal, uint32_t u32_chkBitShiftdVal,
 	*/
 	// This is more efficient
 	return ((u32_inVal >> u8_shiftRightAmt) & u32_chkBitShiftdVal);
+}
+
+// not 100% why this would be needed
+// quoting the author (https://stackoverflow.com/questions/47981/how-to-set-clear-and-toggle-a-single-bit): "Changing the nth bit to x
+// There are alternatives with worse codegen, but the best way is to clear the bit like in bit_clear, then set the bit to value, similar to bit_set. "
+// untested and parameter naming unmodified to conform to mine
+uint32_t bit_set_to(uint32_t number, uint32_t n, bool x)
+{
+    return (number & ~((uint32_t) 1 << n)) | ((uint32_t) x << n);
 }
