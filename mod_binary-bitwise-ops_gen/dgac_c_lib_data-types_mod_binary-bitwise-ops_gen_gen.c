3c61e3d71217a50ab26fcc87b7cc5322def3d389
SchponzeePlusPlus <61894715+SchponzeePlusPlus@users.noreply.github.com>
2025-10-05 15:33:00 +1100

    Update dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
    - Not sure if my logic is correct, so it will need to be checked first

--- mod_binary-bitwise-ops_gen/dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
+++ mod_binary-bitwise-ops_gen/dgac_c_lib_data-types_mod_binary-bitwise-ops_gen_gen.c
@@ -1,3 +1,17 @@
 /*
  *
  */
+
+uint32_t setShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_setVal, uint8_t u8_shiftLeftAmt)
+{
+	uint32_t result = u32_inVal;
+	result |= (u32_setVal << u8_shiftLeftAmt);
+	return result;
+}
+
+uint32_t clearShiftLeftIntoValUint32(uint32_t u32_inVal, uint32_t u32_clearVal, uint8_t u8_shiftLeftAmt)
+{
+	uint32_t result = u32_inVal;
+	result &= ~(u32_setVal << u8_shiftLeftAmt);
+	return result;
+}
